6fd23d9e2063107747642454a4175c0a3afdd699
Guenzler <148702094+Guenzler@users.noreply.github.com>
2024-02-26 16:42:18 +0100

    bugfix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const { check, validationResult } = require('express-validator');
 
 //mongoose.connect('mongodb://localhost:27017/movieDB');
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express'),
   bodyParser = require('body-parser'),
